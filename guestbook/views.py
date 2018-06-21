0d806de022aa5f3a15ae8cc0f578c2d40e0aa2ae
Robert Neuzil <robertneuzil@gmail.com>
2018-06-20 22:10:51 -0400

    new developments

--- guestbook/views.py
+++ guestbook/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 def index(request):
-	return HttpResponse("<h1> Hello World </h1>")
+	return HttpResponse("<h1> Hellllooooo  </h1>")
+
 
 
 # Create your views here.
