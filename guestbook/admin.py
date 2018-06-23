c5a86b3a9d85a187cbe4c0b13df31eebbf895ac1
Robert Neuzil <robertneuzil@gmail.com>
2018-06-22 23:25:54 -0400

    added forms, users can submit to database

--- guestbook/admin.py
+++ guestbook/admin.py
@@ -2,6 +2,4 @@ from django.contrib import admin
 
 from .models import Comment
 
-admin.site.register(Comment) 
-
-# Register your models here.
+admin.site.register(Comment)
--- guestbook/models.py
+++ guestbook/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.utils import timezone
 
 class Comment(models.Model):
-	name = models.CharField(max_length=25)
-	comment = models.TextField()
-	date_added = models.DateTimeField(default=timezone.now)
+    name = models.CharField(max_length=20)
+    comment = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
 
-	def __str__(self):
-		return f"{self.name} and {self.date_added}"
+    def __str__(self):
+        return '<Name: {}, ID: {}>'.format(self.name, self.id)
 # Create your models here.
